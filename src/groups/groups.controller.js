637bc976f469be37e349519de1013593f5402cdb
Shujaat Ali <shujaat.ali@zigron.com>
2020-08-27 13:52:06 +0500

    fixed the login issue

--- src/groups/groups.controller.js
+++ src/groups/groups.controller.js
@@ -19,9 +19,14 @@ const getAllGroups = (req, res) => {
 };
 
 const addGroup = (req, res) => {
-  const group = req.body;
-  console.log("req.body", req.body);
-  createGroup(group)
+  const { name, email, address } = req.body;
+
+  const created_on = new Date();
+  const query = `INSERT INTO groups (name, email, address, created_on)
+  VALUES ($1, $2, $3, $4) RETURNING *`;
+
+  pool
+    .query(query, [name, email, address, created_on])
     .then((result) => {
       const resultModel = baseModel({
         success: true,
@@ -41,17 +46,63 @@ const addGroup = (req, res) => {
     });
 };
 
-const createGroup = (group) => {
-  const { name, email, address } = group;
+const updateGroup = (req, res) => {
+  const { name, email, address } = req.body;
 
   const created_on = new Date();
   const query = `INSERT INTO groups (name, email, address, created_on)
   VALUES ($1, $2, $3, $4) RETURNING *`;
 
-  return pool.query(query, [name, email, address, created_on]);
+  pool
+    .query(query, [name, email, address, created_on])
+    .then((result) => {
+      const resultModel = baseModel({
+        success: true,
+        message: "groups are fetched successfully.",
+        data: result.rows[0],
+      });
+      res.status(200).json(resultModel);
+    })
+    .catch((error) => {
+      console.log("error", error);
+
+      const resultModel = baseModel({
+        success: false,
+        message: error,
+      });
+      res.status(200).json(resultModel);
+    });
+};
+
+const deleteGroup = (req, res) => {
+  const { id } = req.body;
+
+  const query = `DELETE FROM groups WHERE id = $1 RETURNING *`;
+
+  pool
+    .query(query, [id])
+    .then((result) => {
+      const resultModel = baseModel({
+        success: true,
+        message: "group deleted successfully.",
+        data: result.rows[0],
+      });
+      res.status(200).json(resultModel);
+    })
+    .catch((error) => {
+      console.log("error", error);
+
+      const resultModel = baseModel({
+        success: false,
+        message: error,
+      });
+      res.status(200).json(resultModel);
+    });
 };
 
 module.exports = {
   getAllGroups,
   addGroup,
+  updateGroup,
+  deleteGroup,
 };
